d83cdfe8993b2169e2ffce860b0cbec171279ad9
ilvalle <paolo.valleri@gmail.com>
2015-03-03 21:09:13 +0100

    added proxy to redirect all wms requests to geoserver

--- controllers/default.py
+++ controllers/default.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
-session.forget(response)
+#session.forget(response)
+import requests
 
 #@cache.action(time_expire=3600, cache_model=cache.ram)
 def index():
     return {}
+
+geoserver_url = "http://geodata.integreen-life.bz.it"
+def wms():
+    url="%s/%s" % (geoserver_url, "geoserver/edi/wms")
+    params=request.vars
+    r = requests.get(url, params=params, stream=True)
+    response.headers['Content-Type'] = r.headers.get('content-type')
+    if r.headers.get('content-type').startswith('text'):
+        return r.text
+    else:
+        return response.stream(r.raw)
